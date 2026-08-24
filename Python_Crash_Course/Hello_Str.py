6ab460cb6d3fb221aa01eebe8c65213b31f979b0
Claus Teglman <claus@teglman.dk>
2026-08-24 15:19:57 +0200

    Move Python Crash Course exercises to separate project

--- Python_Crash_Course/Hello_Str.py
+++
@@ -1,7 +0,0 @@
-message="claus teGlman"
-first_name="claus"
-last_name="teGlman"
-full_name=f"{first_name} {last_name}"
-message=f"Hello {full_name.title()} Welcome back!"
-print(message)
-
--- Python_Crash_Course/apostrophe.py
+++
@@ -1,4 +0,0 @@
-message = "One of Python's strengths is its diverse community."
-print(message)
-file_name="python_notes.txt"
-print(file_name.removesuffix(".txt"))
